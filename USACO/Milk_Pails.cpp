e95038f5ebbfb1c633e5d6806686cab5a2e3fc07
Harshit Sati <59113445+HarshitSati@users.noreply.github.com>
2021-07-14 00:42:03 +0530

    Add files via upload

--- USACO/Milk_Pails.cpp
+++ USACO/Milk_Pails.cpp
@@ -3,20 +3,21 @@
 // 1<=M<=1000
 // assumption : X+Y <M
 #include<iostream>
-
+#include<fstream>
 using namespace std;
 
-
+/*
 int main(){
-
+  ifstream fin("pails.in");
+  ofstream fout("pails.out");
   int X,Y,M;
-  cin>>X>>Y>>M;
+  fin>>X>>Y>>M;
   int ans=X+Y;
   if(M >= ans){
     M = M-ans;
   }
   //cout<<M;
-  while(X <= M){
+  while(M>= X){
     M = M-X;
     ans +=X;
     //cout<<ans<<" "<<M<<" ";
@@ -25,5 +26,41 @@ int main(){
       ans +=Y;
     }
   }
-  cout<<ans;
+  fout<<ans;
+
+}*/
+/*int main(){
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  ifstream fin("pails.in");
+  ofstream fout("pails.out");
+  int X,Y,M;
+  fin>>X>>Y>>M;
+  int a = M/X;
+  int b = M/Y;
+  int  ans=0;
+  for(int i = 0 ; i < a; i++ ){
+    for(int j = 0; j < b; j++){
+      int n = X*i + Y*j;
+      if(n>M) break;
+      ans = max(ans, n);
+    }
+  }
+  fout<<ans;
+}*/
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+  ifstream fin("pails.in");
+  ofstream fout("pails.out");
+	int a,b,c,ans=0;
+	fin >> a >> b >> c;
+	for (int i = 0; i < 1001; ++i) {
+		if (a*i > c) break;
+		for (int j = 0; j < 1001; ++j) {
+			int n = a*i + b*j;
+			if (n > c) break;
+			ans = max(ans,n);
+		}
+	}
+	fout << ans;
+	return 0;
 }
